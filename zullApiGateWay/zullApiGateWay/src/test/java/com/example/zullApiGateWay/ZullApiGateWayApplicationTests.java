27c7dd1148cba719ddb104e4373c75276e90a414
8788808434 <AP4447628@GMAIL.COM>
2021-05-10 18:28:08 +0530

    new changes

---
+++ zullApiGateWay/zullApiGateWay/src/test/java/com/example/zullApiGateWay/ZullApiGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zullApiGateWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZullApiGateWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
